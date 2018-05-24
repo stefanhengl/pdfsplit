6c2918aaa1670e7ee0104d265b2a92d31cbff72c
D055847 <stefan.hengl@sap.com>
2018-05-24 11:37:50 +0200

    bump version to release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pdfsplit "0.4.0-SNAPSHOT"
+(defproject pdfsplit "0.4.0"
   :description "A Clojure library that splits and merges (multi-page) PDFs."
   :url "https://github.com/stefanhengl/pdfsplit"
   :scm {:name "git" :url "https://github.com/stefanhengl/pdfsplit" }
@@ -8,5 +8,7 @@
                  [org.clojure/clojure "1.9.0"]
                  [org.apache.pdfbox/pdfbox "2.0.8"]
                  ]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]]
   )
 
